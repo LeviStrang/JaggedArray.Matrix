318da07ec938c77c7810840c60bb03afc51e5202
Levi Strang <LeviStrang@outlook.com>
2018-10-25 11:43:12 -0400

    Add project files.

---
+++ JaggadArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray.Matrix
+{
+    class JaggedArray
+    {
+       public static void AddMatrix()
+        {
+            int[][] Matrix = new int[][]
+            {
+                new int[] { 0, 2, 3, 2 },
+                new int[] { 0, 6, 0, 1 },
+                new int[] { 4, 0, 3, 0 }
+            };
+            int x = 0;
+            for (int i = 0; i < Matrix[0].Length; i++)
+            {
+                for (int j = 0; j < Matrix.Length && Matrix[j][i] > 0; j++)
+                {
+                    x += Matrix[j][i];
+                }
+            }
+            Console.WriteLine(x);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JaggedArray;
+
+namespace JaggedArray.Matrix
+{
+    class Program : JaggedArray
+    {
+        static void Main(string[] args)
+        {
+            AddMatrix();
+        }
+    }
+}
